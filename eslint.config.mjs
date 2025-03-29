43a831b8c28d0ece4f7da7a7854d6e0b10e53da3
NIkita <nblohin97@gmail.com>
2025-03-29 22:42:45 +1000

    на ревью

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,22 +6,20 @@ export default tseslint.config(
     tseslint.configs.recommended,
     {
         rules: {
-            indent: ["error", 4],
-            semi: ["error", "always"],
-            quotes: ["error", "single"],
-            "no-console": "off",
+            indent: ['error', 4],
+            semi: ['error', 'always'],
+            quotes: ['error', 'single'],
+            'no-console': 'off',
 
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-unused-vars": "off",
-            "@typescript-eslint/no-unsafe-function-type": "off",
-            "@typescript-eslint/ban-ts-comment": "off",
-            "@typescript-eslint/no-this-alias": "off",
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/no-unsafe-function-type': 'off',
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/no-this-alias': 'off',
 
-            "no-unused-expressions": "error",
-            "prefer-const": "error",
-            "no-var": "error",
-
-            "import/extensions": "off"
+            'no-unused-expressions': 'error',
+            'prefer-const': 'error',
+            'no-var': 'error',
         }
     }
 );
